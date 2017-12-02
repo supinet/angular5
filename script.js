d1509b9bb1bea6e44c9aab2bc0614c0a9f1efc3c
supinet <fernando.supi@gmail.com>
2017-12-02 12:54:11 -0400

    test

---
+++ script.js
@@ -0,0 +1,5 @@
+(function () {
+    var nome = document.getElementById('nome');
+    var sobrenome = document.getElementById('sobrenome');
+    alert("Ol\u00E1 Sr: " + nome + " " + sobrenome);
+});
---
+++ script.ts
@@ -0,0 +1,5 @@
+() => {
+    let nome = document.getElementById('nome');
+    let sobrenome = document.getElementById('sobrenome');
+    alert(`Olá Sr: ${nome} ${sobrenome}`);
+}
